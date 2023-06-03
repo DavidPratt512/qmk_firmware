4623aae65b9b5d3c512c97040a81f7b3f31a3226
David Pratt <davidpratt512@gmail.com>
2023-06-02 22:16:57 -0700

    Remove unused config options

--- keyboards/keebio/iris/keymaps/davidpratt512/config.h
+++ keyboards/keebio/iris/keymaps/davidpratt512/config.h
@@ -1,9 +1,4 @@
 #pragma once
 
-
-/* -- TapDance --------------------------------------------------------------------- */
-
 #define TAPPING_TERM 175          /* Timeout for TapDance and ModTaps */
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
-#define ONESHOT_TAP_TOGGLE 2
-#define ONESHOT_TIMEOUT 1500
