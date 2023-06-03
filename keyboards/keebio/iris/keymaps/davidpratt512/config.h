7671d92b5037a0e9136dbc2d63904c07b3f2e742
David Pratt <davidpratt512@gmail.com>
2023-06-02 22:39:28 -0700

    Inconsequential formatting and name changes

--- keyboards/keebio/iris/keymaps/davidpratt512/config.h
+++ keyboards/keebio/iris/keymaps/davidpratt512/config.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define TAPPING_TERM 175          /* Timeout for TapDance and ModTaps */
-#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+#define TAPPING_TERM 175                 /* Timeout for and mod taps, layer taps, etc     */
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY  /* Specify get-hold config on a key-by-key basis */
--- keyboards/keebio/iris/keymaps/davidpratt512/keymap.c
+++ keyboards/keebio/iris/keymaps/davidpratt512/keymap.c
@@ -4,11 +4,11 @@
 /* ---------------------------- Declare Custom Keycodes ---------------------------- */
 
 enum layers {
-    _QWERTY,
-    _STURDY,
-    _LOWER,
-    _RAISE,
-    _NUMPAD,
+    L_QWERTY,
+    L_STURDY,
+    L_LOWER,
+    L_RAISE,
+    L_NUMPAD,
 };
 
 enum combos {
@@ -18,27 +18,27 @@ enum combos {
 
 uint16_t COMBO_LEN = COMBO_LENGTH;
 
-#define TAB_CTL LCTL_T(KC_TAB)
-#define QUT_CTL RCTL_T(KC_QUOT)
-#define LWR_ENT LT(_LOWER, KC_ENT)
-#define RAIS_BS LT(_RAISE, KC_BSPC)
-#define ESC_NUM LT(_NUMPAD, KC_ESC)
-#define GME_LFT G(S(KC_PGUP))
-#define GME_RGT G(S(KC_PGDN))
-#define DF_QWER DF(_QWERTY)
-#define TRM_CPY C(S(KC_C))
-#define TRM_PST C(S(KC_V))
+#define TAB_CTL LCTL_T(KC_TAB)        /* Tap for tab, hold for control               */
+#define QUT_CTL RCTL_T(KC_QUOT)       /* Tap for quote, hold for control             */
+#define LWR_ENT LT(L_LOWER, KC_ENT)   /* Tap for enter, hold for L_LOWER             */
+#define RAIS_BS LT(L_RAISE, KC_BSPC)  /* Tap for bksp, hold for L_RAISE              */
+#define ESC_NUM LT(L_NUMPAD, KC_ESC)  /* Tap for esc, hold for L_NUMPAD              */
+#define GNME_LF G(S(KC_PGUP))         /* Shift window to prev workspace (gnome)      */
+#define GNME_RT G(S(KC_PGDN))         /* Shift window to next workspace (gnome)      */
+#define DF_QWER DF(L_QWERTY)          /* Set default layer to L_QWERTY               */
+#define TERM_CP C(S(KC_C))            /* Terminal copy (ctrl + shift + c)            */
+#define TERM_PT C(S(KC_V))            /* Terminal paste (ctrl + shift + v)           */
 
 /* STURDY-specific keycodes */
-#define DF_STRD DF(_STURDY)
-#define SLH_CTL RCTL_T(KC_SLSH)
+#define DF_STRD DF(L_STURDY)          /* Set default layer to L_STURDY               */
+#define SLH_CTL RCTL_T(KC_SLSH)       /* Tap for slash, hold for control             */
 
 
 /* ------------------------------------- Layers ------------------------------------ */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT(
+  [L_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_LALT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RALT,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -48,11 +48,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_VOLD,          KC_VOLU, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    TRM_CPY, ESC_NUM, LWR_ENT,                   KC_SPC,  RAIS_BS, TRM_PST
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+                                    TERM_CP, ESC_NUM, LWR_ENT,                   KC_SPC,  RAIS_BS, TERM_PT
+  //                               └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_STURDY] = LAYOUT(
+  [L_STURDY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_LALT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RALT,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -62,11 +62,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_K,    KC_Q,    KC_G,    KC_W,    KC_VOLD,          KC_VOLU, KC_B,    KC_H,    KC_QUOT, KC_COLN, KC_COMM, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    TRM_CPY, ESC_NUM, LWR_ENT,                   KC_SPC,  RAIS_BS, TRM_PST
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+                                    TERM_CP, ESC_NUM, LWR_ENT,                   KC_SPC,  RAIS_BS, TERM_PT
+  //                               └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_LOWER] = LAYOUT(
+  [L_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -77,24 +77,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, DF_QWER,          DF_STRD, _______, KC_PIPE, KC_LABK, KC_RABK, KC_BSLS, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, KC_DEL,  _______
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  //                               └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_RAISE] = LAYOUT(
+  [L_RAISE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, G(KC_1), G(KC_2), G(KC_3), G(KC_4), G(KC_5),                            G(KC_6), G(KC_7), G(KC_8), G(KC_9), _______, QK_BOOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______,                            _______, GME_LFT, GME_RGT, _______, _______, _______,
+     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______,                            _______, GNME_LF, GNME_RT, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, _______, KC_GRV,  KC_TILD, _______, _______,          _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  //                               └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_NUMPAD] = LAYOUT(
+  [L_NUMPAD] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______, _______, _______, _______, _______, _______, _______,          _______, _______, KC_1,    KC_2,    KC_3,    _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, KC_0,    _______
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  //                               └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
 };
@@ -115,7 +115,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     /**
-     * Return false for a keycode as to *not* select the hold action on other keypress.
+     * Return false for a keycode as to *not* select the hold action on other 
+     * keypress.
+     *
      * Return true for a keycode as to immediately select the hold action on other
      * keypress.
      */
@@ -136,35 +138,38 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM pinky_caps_combo[] = { KC_LSFT, KC_RSFT, COMBO_END };
 
 combo_t key_combos[] = {
+
+    /* Tap both shift keys to toggle caps lock */
     [PINKY_CAPS] = COMBO(pinky_caps_combo, KC_CAPS),
+
 };
 
 
 /* --------------------------------- Key Overrides --------------------------------- */
 
 const key_override_t comma_paren_override = ko_make_with_layers(
-        MOD_MASK_SHIFT,  /* When shift is active            */
-        KC_COMM,         /* and KC_COMM is pressed          */
-        KC_LPRN,         /* replace the result with KC_LPRN */
-        1 << _QWERTY     /* on these layers.                */
+    MOD_MASK_SHIFT,  /* When shift is active            */
+    KC_COMM,         /* and KC_COMM is pressed          */
+    KC_LPRN,         /* replace the result with KC_LPRN */
+    1 << L_QWERTY    /* on these layers                 */
 );
 
 const key_override_t dot_paren_override = ko_make_with_layers(
-        MOD_MASK_SHIFT,  /* When shift is active            */
-        KC_DOT,          /* and KC_DOT is pressed           */
-        KC_RPRN,         /* replace the result with KC_RPRN */
-        1 << _QWERTY     /* on these layers.                */
+    MOD_MASK_SHIFT,  /* When shift is active            */
+    KC_DOT,          /* and KC_DOT is pressed           */
+    KC_RPRN,         /* replace the result with KC_RPRN */
+    1 << L_QWERTY    /* on these layers                 */
 );
 
 const key_override_t colon_semicolon_override = ko_make_basic(
-        MOD_MASK_SHIFT,  /* When shift is active            */
-        KC_COLN,         /* and KC_COLN is pressed          */
-        KC_SCLN          /* replace the result with KC_SCLN */
+    MOD_MASK_SHIFT,  /* When shift is active            */
+    KC_COLN,         /* and KC_COLN is pressed          */
+    KC_SCLN          /* replace the result with KC_SCLN */
 );
 
 const key_override_t **key_overrides = (const key_override_t *[]) {
-    &comma_paren_override,
-    &dot_paren_override,
-    &colon_semicolon_override,
-    NULL  /* The array must be null-terminated */
+    &comma_paren_override,      /* Use ,/( instead of ,/<  */
+    &dot_paren_override,        /* Use ./) instead of ./>  */
+    &colon_semicolon_override,  /* Use :/; instead of ;/:  */
+    NULL                        /* Must be null-terminated */
 };
