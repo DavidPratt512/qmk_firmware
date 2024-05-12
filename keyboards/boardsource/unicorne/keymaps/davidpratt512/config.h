140cd3d3214d429162cb7fb2e709d8155b694496
David Pratt <davidpratt512@gmail.com>
2024-05-11 20:39:16 -0700

    unicorne: start

---
+++ keyboards/boardsource/unicorne/keymaps/davidpratt512/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// use usb-c connection on the right half of the board
+#define MASTER_RIGHT
+
+// Timeout for mod taps, layer taps, etc
+#define TAPPING_TERM 175
+
+// Specify get-hold config on a key-by-key basis
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
